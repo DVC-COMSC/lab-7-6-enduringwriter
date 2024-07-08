c1a3cd2e49a08969feecc4dd8a7a6ea5e11f5947
Keith Stateson <113780814+enduringwriter@users.noreply.github.com>
2024-07-08 21:29:20 +0000

    Lab7-6 Reverse List of Numbers

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-
 def getInput():
     """
-    ########################################
-    Code Your Program here
-    ########################################
+    Get integers from user in a single line.
+    Split the values and conver them into a list of integers, and return it.
     """
+    return list(map(int, input("Enter a list of numbers. Press 'enter' when done.").split()))
 
 
 def makeReverse(numbers):
     """
-    ########################################
-    Code Your Program here
-    ########################################
+    Create a new list of numbers in rev order without using rev function.
     """
+    new_numbers = []
+    for n in numbers:
+        new_numbers.insert(0, n)
+    return new_numbers
 
 
 def main():
